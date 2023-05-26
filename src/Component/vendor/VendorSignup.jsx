741471c712c84a14f58d434f1b5d9bb66dd5562a
webdeveloper510 <developerweb6@gmail.com>
2023-05-26 11:25:30 +0530

    changes

--- src/Component/vendor/VendorSignup.jsx
+++ src/Component/vendor/VendorSignup.jsx
@@ -206,17 +206,17 @@ function VendorSignup() {
                 <h3 className='mb-4'>Merchant Sign Up</h3>
                 <form className='d-flex flex-wrap justify-content-between align-items-center w-100'>
                 <div className="input-field">
-                        <label className='text-start w-100 mb-2 text-dark'>Username</label>
+                        <label className='text-start w-100 mb-2 text-dark'>Username<strong style={{color: 'red'}}>*</strong></label>
                         <input type="text" className='mb-0' maxLength='30' value={name} onChange={handleName} />
                     </div>
                     
                     <div className="input-field">
-                        <label className='text-start w-100 mb-2 text-dark'>Email</label>
+                        <label className='text-start w-100 mb-2 text-dark'>Email<strong style={{color: 'red'}}>*</strong></label>
                         <input type="email" maxLength='35' className='mb-0' value={email} onChange={handleEmail} />
                     </div>
                     
                     <div className="input-field position-relative">
-                        <label className='text-start w-100 mb-2 text-dark'>Password</label>
+                        <label className='text-start w-100 mb-2 text-dark'>Password<strong style={{color: 'red'}}>*</strong></label>
                         <input type={showPassword ? 'text' : 'password'} maxLength='30' className='mb-0' value={password} onChange={handlePassword} />
                         <FontAwesomeIcon
                             icon={faEye}
@@ -230,7 +230,7 @@ function VendorSignup() {
                     </div>
                     
                     <div className="input-field position-relative">
-                        <label className='text-start w-100 mb-2 text-dark'>Confirm Password</label>
+                        <label className='text-start w-100 mb-2 text-dark'>Confirm Password<strong style={{color: 'red'}}>*</strong></label>
                         <input type={confirmType ? 'text' : 'password'} className='mb-0' value={confirmPassword} onChange={handleConfirmPassword} />
                         <FontAwesomeIcon
                             icon={faEye}
@@ -254,7 +254,7 @@ function VendorSignup() {
                     </div>
 
                     <div className="input-field">
-                        <label className='text-start w-100 mb-2 text-dark'>Shopify URL</label>
+                        <label className='text-start w-100 mb-2 text-dark'>Shopify URL<strong style={{color: 'red'}}>*</strong></label>
                         <div className="input-container w-100 d-flex mb-0">
                             <label htmlFor="">https://</label>
                             <input type="url" className='mb-0' pattern="https?://.*" placeholder='shopify URL' value= {shopifyUrl} onChange={handleShopify} />
