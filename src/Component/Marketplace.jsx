90fc03d5704f5a295a79e70011deedcefc2a0ea9
developer <developerweb6@gmail.com>
2023-08-03 11:43:48 +0530

    changes

--- src/Component/Marketplace.jsx
+++ src/Component/Marketplace.jsx
@@ -104,11 +104,9 @@ function Marketplace () {
                                                   Product Name: <strong>{prod.product_name}</strong>
                                                   </h6>
 
-                                                  <p className="card-text">Coupon: <strong>{(prod.coupon_name != null ? prod.coupon_name.filter(Boolean).join(", ") : 'No Coupons')}</strong></p>
+                                                  <p className="card-text">Influencer Fee: <strong>{(item.influencer_fee)}</strong></p>
 
-                                                  <p className="card-text">Amount: <strong>{(prod.amount != null ? prod.amount : 'No Coupons').filter(Boolean).join(", ")}</strong></p>
-
-                                                  <p className="card-text">Discount Type: <strong>{(prod.discount_type != null ? prod.discount_type : 'No Coupons')}</strong></p>
+                                                  <p className="card-text">Offer Type: <strong>{(item.offer != null ? item.offer == 'commission' ? 'Commission' : 'Percentage' : 'No Coupons')}</strong></p>
 
                                                   <button className="buttonfx angleindouble color-2" onClick={(e) => {handleApplied(e, item.campaignid_id)}}>Apply</button>
                                               </div>
