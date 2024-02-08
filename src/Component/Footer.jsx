8c76cd606c2bda733fb1fef93b52e76d1f72beca
abcdiksha <diksha_sharma@codenomad.net>
2024-02-08 14:59:08 +0530

    done

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,151 +3,136 @@ import { NavLink} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import logo from '../assets/logo.png';
-import social1 from '../assets/social1 (1).png';
-import social2 from '../assets/social1 (2).png';
-import social3 from '../assets/social1 (3).png';
-import social4 from '../assets/social1 (4).png';
+
 function Footer (){
     return (
         <>
-<div className='newslteer'>
-  <div className='container'>
-<div className='suscribe-news'>
-  <div className='row  align-center'>
-    <div className='col-md-6'>
-<h2>Subscribe to Newsletter</h2>
-<p>Ipsum vel nobis doloremque est aut non accusantium vero <br></br>molestias. Et est minima dolorem eum modi atque sint nobis. </p>
-    </div>
-    <div className='col-md-6'>
-<div className="joinn-div">
-  <input type="text" placeholder='Enter email address'></input>
-  <button className='Join'>Join</button>
-</div>
-</div>
-  </div>
 
-</div>
-  </div>
-
-</div>
 <footer className="footer-area pt-65">
   <div className="container">
     <div className="row justify-content-center">
       <div className="col-md-12">
         
-        <div className="row1">
-          <div className='col-20'>
-          <div className="footer-logo">
+        <div className="row">
+        <div className="col-md-3">
+        <div className="footer-logo">
           <a href="#">
             <img src={logo} alt="Logo" />
           </a>
           <p>Move work forward faster with no-code apps that perfectly match your team's agility and scale.</p>
         </div>
-          </div>
-          <div className='col-20'>
-          <h3 class="footer-heading">Company</h3>
-          <ul className="menu-footer">
+        </div>
+        <div className="col-md-3">
+        <div className="widget footer-menu">
+            <h3>ABOUT US</h3>
+        <ul className="menu">
               <li>
-              <NavLink className="" to="/">About</NavLink>
+              <NavLink className="" to="/">Home</NavLink>
               </li>
               <li>
-                <NavLink to="/product"> Features </NavLink>
+                <NavLink to="/product"> Product </NavLink>
                 </li>
                 <li>
-                <NavLink to="/marketplace"> Works          
-</NavLink>
+                <NavLink to="/marketplace"> Market Place </NavLink>
                 </li>
                 <li>
-                <NavLink to="/resources"> Career </NavLink>
+                <NavLink to="/resources"> Resources </NavLink>
                 </li>
-               
+                <li>
+                <NavLink to="/"> Contact Us </NavLink>
+                </li>
             </ul>
-
-          </div>
-          <div className='col-20'>
-          <h3 class="footer-heading">Help</h3>
-          <ul className="menu-footer">
+        </div>
+        </div>
+        <div className="col-md-3">
+        <div className="widget footer-menu">
+        <h3>OUR COMPANY</h3>
+        <ul className="menu">
               <li>
-              <NavLink className="" to="/">Customer Support
-</NavLink>
+              <NavLink className="" to="/">Home</NavLink>
               </li>
               <li>
-                <NavLink to="/product"> Terms & Conditions </NavLink>
+                <NavLink to="/product"> Product </NavLink>
                 </li>
                 <li>
-                <NavLink to="/marketplace"> Delivery Details</NavLink>
+                <NavLink to="/marketplace"> Market Place </NavLink>
                 </li>
                 <li>
-                <NavLink to="/resources"> 
-Privacy Policy </NavLink>
+                <NavLink to="/resources"> Resources </NavLink>
                 </li>
-              
+                <li>
+                <NavLink to="/"> Shopify apps </NavLink>
+                </li>
+                <li>
+                <NavLink to="/"> Our Services </NavLink>
+                </li>
             </ul>
-          </div>
-          {/* <div className='col-20'>
-          <h3 class="footer-heading">Resources</h3>
-          <ul className="menu-footer">
+        </div>
+        </div>
+        <div className="col-md-3">
+        <div className="widget footer-menu">
+        <h3>PRODUCT</h3>
+        <ul className="menu">
               <li>
-              <NavLink className="" to="/">Free eBooks</NavLink>
+              <NavLink className="" to="/">Features</NavLink>
               </li>
               <li>
-                <NavLink to="/"> Development Tutorial </NavLink>
+                <NavLink to="/"> Videos </NavLink>
                 </li>
                 <li>
-                <NavLink to="/"> How to - Blog </NavLink>
+                <NavLink to="/"> Pricing </NavLink>
                 </li>
                 <li>
-                <NavLink to="/"> Youtube Playlist </NavLink>
+                <NavLink to="/"> Resources </NavLink>
                 </li>
             </ul>
-          </div> */}
-          <div className='col-20'>
-          <h3 class="footer-heading">Contact Us</h3>
-<div className='row'>
-  <div className='col-md-con'>
-<h3>Call us</h3>
-<a href="tel:(+1) 456-493">(+1) 456-4933</a>
-  </div>
-  <div className='col-md-con'>
-<h3>Email us</h3>
-<a  href="mailto:mail@example.com">mail@example.com</a>
-
-</div>
-
-</div>
-</div>
-      
-       
-      
+        </div>
+        </div>
+        
+        </div>
+        <div className="footer-social">
+          <ul>
+            <li>
+              <a href="#">
+                <i className="icofont-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="icofont-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="icofont-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="icofont-google-plus"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="icofont-linkedin"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
-  <div className='footer-border'></div>
-<div className='copyright-div'>
-<div className='row'>
-<div className='col-md-6'>
-<div class="footer-socialicon">
-  Follow Us:
-  <ul>
-  <li><a href="/"><img src={social1}></img></a></li>
-  <li><a href="/"><img src={social2}></img></a></li>
-  <li><a href="/"><img src={social3}></img></a></li>
-
-    <li><a href="/"><img src={social4}></img></a></li>
-  </ul>
-</div>
-</div>
-<div className='col-md-6'>
-  <span class="footer-align">© Copyright 2024, All Rights Reserved</span>
-
-
-</div>
-</div>
-</div>
-</div>
-
-
-
+  <div className="copyright pt-18pb-5">
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="copy-right">
+            <p>© 2022 affiliate. All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
       </>
        
--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,29 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { NavLink} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import logo from '../assets/logo.png';
-import menuicon from'../assets/hamburger.png';
 import { Link } from 'react-router-dom';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+
 function Header (){
-  const [show, setShow] = useState(false);
-const handleClose = () => setShow(false);
-const mobilemenuShow = () => setShow(true);
     return (
         <>
 
-<header className="header-area absulate-header cssanimation  navbar-fixed-top sticky fadeInTop animated">
+<header className="header-area absulate-header cssanimation d-none d-xl-block navbar-fixed-top sticky fadeInTop animated">
   <div className="container">
     <div className="row">
-      <div className="col-md-2">
+      <div className="col-9 col-md-3">
         <div className="site-logo">
           <Link to="/">
             <img src={logo} alt="Logo" />
           </Link>
         </div>
       </div>
-      <div className="col-md-7 mobile-hide">
+      <div className="col-md-3 col-md-9">
         <div className="main-menu-wrap">
           <nav className="proone-nav">
             <ul className="menu">
@@ -46,76 +42,13 @@ const mobilemenuShow = () => setShow(true);
                 <li>
                 <NavLink to="/marketplace"> Marketplace </NavLink>
                 </li>
-               
-
-            </ul>
-          </nav>
-        </div>
-        
-        <div className="mobile-menu-area"></div>
-      </div>
-      <div className="col-md-3 mobile-hide">
-      <nav className="proone-nav">
-      <ul className="menu button-login-si">
                {localStorage.getItem("logToken") == null ? (
                 <>
                 <li>
                 <NavLink to="/login" className='login'> Login</NavLink>
                 </li>
                 <li>
-                <NavLink to="/signup" className='buttonfx angleindouble color-1 Signup'> <i class="icofont-sign-in"></i> Sign Up</NavLink>
-                </li>
-                </>
-               ) : (
-                   <></>
-               )
-                
-              }
-              </ul>
-              </nav>
-               </div>
-               <div className='col-md-10 desktop-hide'>
-               <div className="mobile-menu"> <img src={menuicon} onClick={mobilemenuShow}></img> </div>
-               </div>
-
-               <Offcanvas show={show} onHide={handleClose}>
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title> <div className="logo">
-              <div className="site-logo">
-          <Link to="/">
-            <img src={logo} alt="Logo" />
-          </Link>
-        </div>
-              </div></Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-            <ul className="mobile-menu">
-              <li>
-              <NavLink className="" to="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/product"> Product </NavLink>
-                </li>
-                <li>
-                <NavLink to="/vendor-signup"> Vendor Signup </NavLink></li>
-                {/* <span> Market Place </span> */}
-                {/* <ul class="subpage">
-                <li><NavLink to="/marketplace"> Home </NavLink></li>
-                <li><NavLink to="/offers"> Offers </NavLink></li>
-                <li><NavLink to="/products"> Products </NavLink></li>
-                <li><NavLink to="/vendor-signup"> Vendor Login/  Signup </NavLink></li>
-                </ul> */}
-                {/* </li> */}
-                <li>
-                <NavLink to="/marketplace"> Marketplace </NavLink>
-                </li>
-                {localStorage.getItem("logToken") == null ? (
-                <>
-                <li>
-                <NavLink to="/login" className='login'> Login</NavLink>
-                </li>
-                <li>
-                <NavLink to="/signup" className=''> <i class="icofont-sign-in"></i> Sign Up</NavLink>
+                <NavLink to="/signup" className='buttonfx angleindouble color-1 Signup'> <i class="icofont-sign-in"></i> SignUp</NavLink>
                 </li>
                 </>
                ) : (
@@ -125,14 +58,13 @@ const mobilemenuShow = () => setShow(true);
               }
 
             </ul>
-            </Offcanvas.Body>
-          </Offcanvas>
+          </nav>
+        </div>
+        <div className="mobile-menu-area"></div>
+      </div>
     </div>
   </div>
 </header>
-<div className='spacer-div'>
-
-</div>
       </>
        
     ) ;
